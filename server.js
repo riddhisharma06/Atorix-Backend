3a22a72dbedbe7b4819cc5c38d0cc03de41a64e9
riddhisharma06 <riddhi.atorix@gmail.com>
2025-12-18 17:47:28 +0530

    Fix port for Render Deployment

--- server.js
+++ server.js
@@ -558,7 +558,8 @@ app.use((err, req, res, next) => {
 });
 
 // --- Start Server ---
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is listening intently on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
